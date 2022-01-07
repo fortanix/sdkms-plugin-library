e9f1b257e699b900f858162e1d3e9b6a8ec5f347
devansh <devansh.bansal@fortanix.com>
2022-01-07 15:42:45 +0530

    Update azure-hsm plugin to ensure azure error is forwarded to user

--- azure-hsm/plugin.lua
+++ azure-hsm/plugin.lua
@@ -1,9 +1,10 @@
 -- Name: Self-Defending KMS-Azure Bring Your Own Key (BYOK) HSM
--- Version: 1.0
--- Description:## Short Description
--- This plugin implements the Bring your own key (BYOK) HSM model for Azure cloud. Using this plugin you can keep your key inside Fortanix Self-Defending KMS and use BYOK features of Azure key vault.
+-- Version: 1.1
+-- Description: This plugin implements the Bring your own key (BYOK) HSM model for Azure cloud. Using this plugin you can keep your key inside Fortanix Self-Defending KMS and use BYOK features of Azure key vault.
+-- # FORTANIX SELF-DEFENDING KMS-AZURE BRING YOUR OWN KEY (BYOK) HSM PLUGIN
 -- 
 -- ## Introduction
+-- 
 -- The cloud services provide many advantages but the major disadvantage of cloud providers has been security because physically your data resides with the cloud provider. To keep data secure in a cloud provider environment, enterprises use encryption. So securing their encryption keys become significantly important. Bring Your Own Key (BYOK) allows enterprises to encrypt their data and retain control and management of their encryption keys. This plugin provides an implementation to use the Azure cloud BYOK model.
 -- 
 -- ## Requirenment
@@ -64,7 +65,7 @@
 -- 
 -- This operation will create an RSA key in Fortanix Self-Defending KMS and impot it in Azure key vault.
 -- 
--- ### Parameters
+-- #### Parameters
 -- 
 -- * `operation`: The operation which you want to perform. A valid value is `create`
 -- * `key_name`: Name of the key
@@ -110,18 +111,18 @@
 --   }
 -- ```
 -- 
--- ### List Key operation
+-- #### List Key operation
 -- 
 -- This operation will list all the BYOK keys from azure.
 -- 
--- ### Parameters
+-- #### Parameters
 -- 
 -- * `operation`: The operation which you want to perform. A valid value is `list`.
 -- * `key_name`: Name of the key
 -- * `key_vault`: Azure key vault name
 -- * `secret_id`: The response of `configuration` operation. 
 -- 
--- ### Example
+-- #### Example
 -- 
 -- Input JSON
 -- ```
@@ -161,7 +162,7 @@
 -- 
 -- This operation will delete a key in Fortanix Self-Defending KMS as well as Azure key vault.
 -- 
--- ### Parameters
+-- #### Parameters
 -- 
 -- * `operation`: The operation which you want to perform. A valid value is `delete`.
 -- * `key_name`: Name of the key
@@ -215,10 +216,10 @@
 -- 
 -- ## References
 -- - [Azure HSM BYOK](https://docs.microsoft.com/en-us/azure/key-vault/keys/hsm-protected-keys)
--- 
--- ### Release Notes
+--
+-- ## Release Notes
 -- - Initial release
-
+-- - Better error handling
 --[[
 configure operation
 {
@@ -281,7 +282,7 @@ function save_credentials(tenant_id, client_id, client_secret)
   local name = Blob.random { bits = 64 }:hex()
   local secret, err = Sobject.import{ name = name, obj_type = 'SECRET', value = Blob.from_bytes(client_secret), custom_metadata = {['TenantId'] = tenant_id, ['ClientId'] = client_id }}
   if secret == nil then
-    err.message = err.message .. '. fail to configure Azure credential'
+    err.message = err.message .. '. Failed to configure Azure credentials'
     return {secret_id = nil, error = err}
   end
   return {secret_id = secret.kid, error = nil}
@@ -290,13 +291,13 @@ end
 function login(secret_id)
   local sobject, err = Sobject { id = secret_id }
   if sobject == nil then
-    return {result = nil, error = err, message = "Azure cloud credential is not configure or invalid. Please run configure operation."}
+    return {result = nil, error = err, message = "Azure cloud credentials are not configured, or invalid. Please run configure operation."}
   end
   if sobject.custom_metadata == nil then
-    return {result = nil, error = err, message = "Azure cloud credential is not configure or invalid. Please run configure operation."}
+    return {result = nil, error = err, message = "Azure cloud credentials are not configured, or invalid. Please run configure operation."}
   end
   if sobject.custom_metadata['TenantId'] == nil or sobject.custom_metadata['ClientId'] == nil then
-    return {result = nil, error = err, message = "Azure cloud credential is not configure or invalid. Please run configure operation."}
+    return {result = nil, error = err, message = "Azure cloud credentials are not configured, or invalid. Please run configure operation."}
   end
   local client_secret = sobject:export().value:bytes()
   local tenant_id = Sobject { id = secret_id }.custom_metadata['TenantId']
@@ -327,7 +328,7 @@ function config_kek_key(headers, kek_key_kid)
   local encoded_kek_key = prefix ..  prepad_signed(Blob.from_base64(modulus):hex()) ..suffix
   local sobject, err = Sobject.import { name = name, obj_type = 'RSA', value = Blob.from_hex(encoded_kek_key), key_ops = {'EXPORT', 'WRAPKEY'}, transient = true }
   if sobject == nil then
-    err.message = err.message .. '. Fail to import KEK key in SDKMS'
+    err.message = err.message .. '. Failed to import KEK key in DSM.'
     return {result = nil, error = err}
   end
   return {result = sobject, error = nil}
@@ -337,7 +338,7 @@ function create_wrapping_key()
   local name = Blob.random { bits = 64 }:hex()
   local sobject, err = Sobject.create { name = name, obj_type = 'AES', key_size = 256, key_ops = {'EXPORT', 'WRAPKEY'}, transient = true}
   if sobject == nil then
-    err.message = err.message .. '. Fail to create wrapping key in SDKMS'
+    err.message = err.message .. '. Failed to create wrapping key in DSM.'
     return {result = nil, error = err}
   end
   return {result = sobject, error = nil}
@@ -346,7 +347,7 @@ end
 function create_target_key(name)
   local sobject, err = Sobject.create { name = name, obj_type = "RSA", key_size = 2048, key_ops = {'EXPORT'}}
   if sobject == nil then
-    err.message = err.message .. '. Fail to create target key in SDKMS'
+    err.message = err.message .. '. Failed to create target key in DSM.'
     return {result = nil, error = err}
   end
   return {result = sobject, error = nil}
@@ -355,25 +356,25 @@ end
 function wrap_wrapping_key(wrapping_key, kek_key)
   local wrap_response, err = kek_key:wrap { subject = wrapping_key, mode = 'OAEP_MGF1_SHA1', alg = 'RSA'}
   if wrap_response == nil then
-    err.message = err.message .. '. Fail to wrap wrapping key.'
+    err.message = err.message .. '. Failed to wrap wrapping key.'
     return {result = nil, error = err}
   end
   return {result = wrap_response.wrapped_key:bytes(), error = nil}
 end
 
-function wrap_traget_key(wrapping_key, target_key)
+function wrap_target_key(wrapping_key, target_key)
   local wrap_response, err = wrapping_key:wrap { subject = target_key, mode = 'KWP', alg = 'AES', key_format = 'Pkcs8' }
   if wrap_response == nil then
-    err.message = err.message .. '. Fail to wrap target key'
-    return nil, err
+    err.message = err.message .. '. Failed to wrap target key.'
+    return {result = nil, error = err}
   end
   return {result = wrap_response.wrapped_key:bytes(), error = nil}
 end
 
-function generate_byok(wrapped_wrapping_key, wrapped_traget_key, kek_key_kid)
+function generate_byok(wrapped_wrapping_key, wrapped_target_key, kek_key_kid)
   local header = "{'kid': " .. "'" .. kek_key_kid .. "'" .. ", 'alg': 'dir', 'enc': 'CKM_RSA_AES_KEY_WRAP'}"
   local generator = "'" .. TOOL_NAME .. ', Version : ' .. TOOL_VERSION .. '; Fortanix Self-Defending KMS, Firmware version : ' .. FIRMWARE_VERSION .. "'"
-  local ciphertext = "'" .. base64url_encode(wrapped_wrapping_key .. wrapped_traget_key) .."'"
+  local ciphertext = "'" .. base64url_encode(wrapped_wrapping_key .. wrapped_target_key) .."'"
   local byok = "{'schema_version':"..  "'" .. SCHEMA_VERSION .. "'" .. ", 'header': " ..header  .. ", 'ciphertext': ".. ciphertext ..", 'generator': " ..generator .. "}"
   return byok
 end
@@ -396,7 +397,7 @@ function list_keys(headers, key_vault)
   local url = 'https://'.. key_vault ..'.vault.azure.net/keys?api-version=7.0'
   local response, err = request { method = 'GET', url = url, headers = headers, body='' }
   if err ~=nil or response.status ~= 200 then
-    return {result = response, error = err, message = "Something went worng. Can't list the keys."}
+    return {result = response, error = err, message = "Something went wrong. Can't list the keys."}
   end
   return {result = json.decode(response.body), error = nil}
 end
@@ -405,7 +406,7 @@ function delete_key(headers, key_vault, key_name)
   local url = 'https://'.. key_vault ..'.vault.azure.net/keys/'.. key_name ..'?api-version=7.0'
   local response = request { method = 'DELETE', url = url, headers = headers, body='' }
   if err ~=nil or response.status ~= 200 then
-    return {result = response, error = err, message = "Something went worng. Can't delete the key."}
+    return {result = response, error = err, message = "Something went wrong. Can't delete the key."}
   end
   return {result = json.decode(response.body), error = nil}
 end
@@ -441,30 +442,30 @@ end
 function check(input)
   if input.operation == 'configure' then
     if input.tenant_id == nil then
-      return nil, 'input parameter tenant_id require'
+      return nil, 'input parameter tenant_id required'
     end
     if input.client_id == nil then
-      return nil, 'input parameter client_id require'
+      return nil, 'input parameter client_id required'
     end
     if input.client_secret == nil then
-      return nil, 'input parameter client_secret require'
+      return nil, 'input parameter client_secret required'
     end
   elseif input.operation == 'create' then
     if input.key_name == nil then
-      return nil, 'input parameter key_name require'
+      return nil, 'input parameter key_name required'
     end
     if input.key_vault == nil then
-      return nil, 'input parameter key_vault require'
+      return nil, 'input parameter key_vault required'
     end
     if input.secret_id == nil then
-      return nil, 'input parameter secret_id require'
+      return nil, 'input parameter secret_id required'
     end
   end
 end
 
 function run(input)
   if not is_valid(input.operation) then
-    return {result = 'Operation is not valid. Operation value should be one of configure, create, list, delete', error = nil}
+    return {result = nil, error = "Operation is not valid. Operation value should be one of `configure`, `create`, `list`, or `delete`."}
   end
 
   if input.operation == 'configure' then
@@ -477,8 +478,11 @@ function run(input)
     local authorization_header = 'Bearer ' ..resp.result
     headers = {['Content-Type'] = 'application/json', ['Authorization'] = authorization_header}
     if input.operation == 'create' then
-      if is_valid_cloud_key(headers, input.key_vault, input.key_name) or is_valid_sdkms_key(input.key_name) then
-        return "Something went wrong or key already exist in Azure cloud or in SDKMS"
+      if is_valid_cloud_key(headers, input.key_vault, input.key_name) then
+        return "Something went wrong, or the key already exists in Azure cloud."
+      end
+      if is_valid_sdkms_key(input.key_name) then
+        return "Something went wrong, or the key already exists in DSM."
       end
 
       local config_kek_key_resp = config_kek_key(headers, input.kek_key_kid)
@@ -490,7 +494,7 @@ function run(input)
       if wrapping_key.result == nil then
         return wrapping_key
       end
-      -- create taget key; we will import encrypted taget key in azure hsm key vault
+      -- create target key; we will import encrypted target key in azure hsm key vault
       local target_key = create_target_key(input.key_name)
       if target_key.result == nil then
         return target_key
@@ -501,22 +505,22 @@ function run(input)
         return wrapped_wrapping_key
       end
       -- Wrap target key
-      local wrapped_traget_key = wrap_traget_key(wrapping_key.result, target_key.result)
-      if wrapped_traget_key.result == nil then
-        return wrapped_traget_key
+      local wrapped_target_key = wrap_target_key(wrapping_key.result, target_key.result)
+      if wrapped_target_key.result == nil then
+        return wrapped_target_key
       end
-      local byok = generate_byok(wrapped_wrapping_key.result, wrapped_traget_key.result, input.kek_key_kid)
+      local byok = generate_byok(wrapped_wrapping_key.result, wrapped_target_key.result, input.kek_key_kid)
       local response = perform_byok(headers, byok, input.key_name, input.key_vault)
       return response
     elseif input.operation == 'list' then
       return list_keys(headers, input.key_vault)
     elseif input.operation == 'delete' then
       if not is_valid_cloud_key(headers, input.key_vault, input.key_name) then
-        return "Something went wrong or key do not exist in Azure cloud or in SDKMS"
+        return "Something went wrong or key does not exist in Azure cloud or in DSM."
       end
       return delete_key(headers, input.key_vault, input.key_name)
     else
-      return {result = '', error = 'Operation unknown. valid operation is `create` and `configure`'}
+      return {result = '', error = "Operation is not valid. Operation value should be one of `configure`, `create`, `list`, or `delete`."}
     end
   end
 end
