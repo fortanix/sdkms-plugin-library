67261a6095e84d4b97736e7507e0224f45c9897a
devansh <devansh.bansal@fortanix.com>
2022-06-08 19:27:24 +0530

    Add Data Custodian Plugin

---
+++ datacustodian/README.md
@@ -0,0 +1,220 @@
+# FORTANIX DSM DATA CUSTODIAN BRING YOUR OWN KEY (BYOK) PLUGIN
+
+## Introduction
+
+The cloud services provide many advantages but the major disadvantage of cloud providers has been security because physically your data resides with the cloud provider. To keep data secure in a cloud provider environment, enterprises use encryption. So securing their encryption keys become significantly important. Bring Your Own Key (BYOK) allows enterprises to encrypt their data and retain control and management of their encryption keys. This plugin provides an implementation to use the Data Custodian BYOK model.
+
+## Requirenment
+
+- Fortanix DMS Version >= 4.2.1528
+
+## Use cases
+
+The plugin can be used to
+
+- Import a Fortanix DSM key (AES or RSA) into Data Custodian
+- Rotate a key in Fortanix DSM and import the new key version of an existing key into Data Custodian
+
+## Setup
+
+- Create a technical user in data custodian and obtain API Key and Secret for this user
+- Create KEK key in Data Custodian and get its Key Id and Version number
+- Obtain group id of the group in Data Custodian where you want to import keys
+
+## Input/Output JSON object format
+
+### Configure operation
+
+This operation configures Data Custodian Technical User credential in Fortanix DSM and returns a UUID. You need to pass this UUID for other operations. This is a one time process.
+
+* `operation`: The operation which you want to perform. A valid value is `configure`.
+* `base_url`: Base URL for your Data Custodian instance
+* `api_key`: API key for technical user in Data Custodian
+* `secret`: Secret for technical user in Data Custodian. Please note you should have activated technical user's credential and use acticated crendential here
+
+#### Example
+
+Input JSON
+```
+{
+   "operation": "configure",
+   "base_url": "https://api-kms-v2-preprod.datacustodian.cloud.sap",
+   "api_key": "eyJjcmVkZ.....VkNzkzNjEifQ==",
+   "secret": "EIVa......CYa8"
+}
+```
+
+Output JSON
+```
+{
+  "secret_id": "90cc4fdf-db92-4c52-83a5-ffaec726b224"
+}
+```
+
+### Import operation
+
+Create operation will create a new key (RSA or AES) in Fortanix DSM and import it into Data Custodian. The key with the name provided should not already exist in DSM.
+
+#### Parameters
+
+* `operation`: The operation which you want to perform. A valid values is `import`
+* `secret_id`: The response of `configuration` operation
+* `is_exportable`: Whether the imported key in Data Custodian should be exportable or not. Valif values for this are true or false
+* `datacustodian_role`: This is an optional parameter. This specified the Data Custodian role that should be given to the imported key. If not specified, role is set as UNSPECIFIED. For a complete list of value for role, please check Data Custodian documentation
+* `datacustodian_group_id`: Group Id of the group in Data Custodian where this key will be imported
+* `target_key_type`: Key type of the target key that you want to generate in DSM and import into Data Custodian. Valid values are RSA and AES
+* `target_key_name` : Name of the target key. This name will be used for the key in DSM and in Data Custodian
+* `target_key_description`: Description of the key. This description will be added to the imported key in Data Custodian
+* `target_key_size`: Size of target key. For RSA key type valid values are 2048, 3072, 4096 and for AES key type valid values are 128, 192 and 256
+* `target_key_version`: Version number of the target key. This version number will be used for the imported key in Data Custodian
+* `kek_key_id`: Id of the KEK in Data custodian. This key must exist in Data Custodian. BYOK process will download the public key from Data Custodian and import into DSM for wrapping key material
+* `kek_key_version`: Version number of the KEK in Data Custodian
+* `target_key_operations`: This is an optional parameter. If not specified, default values for the key type are used. Valid values for RSA key type are "ENCRYPT", "DECRYPT", "SIGN", "VERIFY", "WRAP", "UNWRAP" and valid values for AES key type are "ENCRYPT", "DECRYPT", "WRAP", "UNWRAP"
+
+#### Example
+
+Input JSON without specifying key operations (use default key operations)
+```
+{
+  "operation": "import",
+  "secret_id": "2b4a35dc-511d-4d1c-ad30-29e57cae7686",
+  "is_exportable": false,
+  "datacustodian_group_id": "5069b28d-a01c-4bc0-97ce-c19064d056c0",
+  "target_key_type" : "RSA",
+  "target_key_name": "test-imported-key-20220517-01",
+  "target_key_description": "Test - This is a test key for BYOK from DSM",
+  "target_key_size": 2048,
+  "target_key_version": 0,
+  "kek_key_id": "a58860d9-e832-433d-9c33-d310dd201adc",
+  "kek_key_version": 0
+}
+```
+
+Input JSON with specific key operations
+```
+{
+  "operation": "import",
+  "secret_id": "2b4a35dc-511d-4d1c-ad30-29e57cae7686",
+  "is_exportable": true,
+  "datacustodian_group_id": "5069b28d-a01c-4bc0-97ce-c19064d056c0",
+  "target_key_type" : "RSA",
+  "target_key_name": "test-imported-key-20220517-20",
+  "target_key_description": "Test - This is a test key for BYOK from DSM",
+  "target_key_size": 2048,
+  "target_key_version": 0,
+  "target_key_operations": ["SIGN", "VERIFY"],
+  "kek_key_id": "a58860d9-e832-433d-9c33-d310dd201adc",
+  "kek_key_version": 0
+}
+```
+
+Output JSON
+```
+{
+  "result": {
+    "exportable": true,
+    "id": "e89b3f06-0cf0-4e40-ba31-1237e638cc56",
+    "name": "test-imported-key-20220517-20",
+    "keystoreContext": {
+      "customerHeld": false
+    },
+    "role": "UNSPECIFIED",
+    "enabled": true,
+    "groupId": "5069b28d-a01c-4bc0-97ce-c19064d056c0",
+    "meta": {
+      "primaryVersion": 0,
+      "beingRestored": false,
+      "templateGenerated": false,
+      "lastUsed": null,
+      "creatorName": "Txxx@service.datacustodian.cloud.sap",
+      "creatorId": "7a869e8c-3b19-4e10-8db8-c0864ed79361",
+      "imported": true,
+      "created": "2022-05-17T04:42:13.151180",
+      "totalVersions": 1
+    },
+    "size": 2048,
+    "description": "This is a test key for BYOK from DSM",
+    "type": "RSA",
+    "operations": [
+      "DECRYPT",
+      "ENCRYPT",
+      "SIGN",
+      "UNWRAP",
+      "VERIFY",
+      "WRAP"
+    ]
+  }
+}
+```
+
+### Rotate operation
+
+Rotate operation will rotate an existing key (RSA or AES) in Fortanix DSM and import the new key into Data Custodian.  For rotate operation the key specified by target_key_name must already exist in DSM and the key specified by datacustodian_key_id must already exist in Data custodian.
+
+#### Parameters
+
+* `operation`: The operation which you want to perform. A valid values is `rotate`
+* `secret_id`: The response of `configuration` operation
+* `datacustodian_key_id`: Key Id of key in Data Custodian which needs to be rotated 
+* `target_key_name` : Name of the target key. This name will be used for the key in DSM and in Data Custodian
+* `target_key_version`: Version number of the target key. This version number will be used for the imported key in Data Custodian
+* `kek_key_id`: Id of the KEK in Data custodian. This key must exist in Data Custodian. BYOK process will download the public key from Data Custodian and import into DSM for wrapping key material
+* `kek_key_version`: Version number of the KEK in Data Custodian
+
+#### Example
+
+Input JSON 
+```
+{
+  "operation": "rotate",
+  "secret_id": "2b4a35dc-511d-4d1c-ad30-29e57cae7686",
+  "datacustodian_key_id": "2504681e-cfd2-44ee-ad63-66211560cc62",
+  "target_key_name": "test-imported-key-20220517-aes-01",
+  "target_key_version": 0,
+  "kek_key_id": "a58860d9-e832-433d-9c33-d310dd201adc",
+  "kek_key_version": 0
+}
+```
+
+Output JSON
+```
+{
+  "result": {
+    "operations": [
+      "DECRYPT",
+      "ENCRYPT",
+      "UNWRAP",
+      "WRAP"
+    ],
+    "meta": {
+      "totalVersions": 3,
+      "creatorId": "7a869e8c-3b19-4e10-8db8-c0864ed79361",
+      "templateGenerated": false,
+      "primaryVersion": 2,
+      "created": "2022-05-18T00:09:58",
+      "imported": true,
+      "lastUsed": null,
+      "beingRestored": false,
+      "creatorName": "Txxxx@service.datacustodian.cloud.sap"
+    },
+    "groupId": "5069b28d-a01c-4bc0-97ce-c19064d056c0",
+    "name": "test-imported-key-20220517-aes-01",
+    "type": "AES",
+    "description": "This is a test key for BYOK from DSM",
+    "id": "2504681e-cfd2-44ee-ad63-66211560cc62",
+    "keystoreContext": {
+      "customerHeld": false
+    },
+    "size": 256,
+    "exportable": true,
+    "enabled": true,
+    "role": "UNSPECIFIED"
+  }
+}
+```
+
+## References
+* [Data Custodian REST API](https://api-kms-v2-preprod.datacustodian.cloud.sap/kms/v2/ui/)
+
+## Release Notes
+ Initial release
---
+++ datacustodian/plugin.lua
@@ -0,0 +1,400 @@
+--[[
+configure operation
+{
+   "operation": "configure",
+   "base_url": "https://api-kms-v2-preprod.datacustodian.cloud.sap",
+   "api_key": "de7becae...88ae6",
+   "secret": "SvU...5"
+}
+
+import operation
+{
+  "operation": "import",
+  "secret_id": "75803498-b97f-430e-8fd3-bfd995ffe958",
+  "is_exportable": false,
+  "datacustodian_role": "SAC_MASTER_KEY",
+  "datacustodian_group_id": "5069b28d-a01c-4bc0-97ce-c19064d056c0",
+  "target_key_type" : "RSA",
+  "target_key_name": "test-key",
+  "target_key_description": "This is a test key for BYOK from DSM",
+  "target_key_size": 2048,
+  "target_key_operations": [ "ENCRYPT", "DECRYPT", "SIGN", "VERIFY", "WRAP", "UNWRAP" ],
+  "target_key_version": 0,
+  "kek_key_id": "a105bd21-57cd-4a8a-9a1d-4bf1b1022ab3",
+  "kek_key_version": 0
+}
+
+rotate operation
+{
+  "operation": "rotate",
+  "secret_id": "2b4a35dc-511d-4d1c-ad30-29e57cae7686",
+  "datacustodian_key_id": "2504681e-cfd2-44ee-ad63-66211560cc62",
+  "target_key_name": "test-imported-key-20220517-aes-01",
+  "target_key_version": 0,
+  "kek_key_id": "a58860d9-e832-433d-9c33-d310dd201adc",
+  "kek_key_version": 0
+}
+
+]]--
+
+function configure_credentials(api_key, secret, base_url)
+  local name = Blob.random { bits = 64 }:hex()
+  local secret, err = Sobject.import{ name = name, obj_type = 'SECRET', value = Blob.from_bytes(secret), custom_metadata = {['api_key'] = api_key, ['base_url'] = base_url }}
+  if secret == nil then
+    err.message = err.message .. '. Failed to configure Data Custodian credentials'
+    return {secret_id = nil, error = err}
+  end
+  return {secret_id = secret.kid, error = nil}
+end
+
+function login(secret_id)
+  local sobject, err = Sobject { id = secret_id }
+  if sobject == nil then
+    return {result = nil, error = err, message = "Data Custodian credentials are not configured, or invalid. Please run configure operation."}
+  end
+  if sobject.custom_metadata == nil then
+    return {result = nil, error = err, message = "Data Custodian credentials are not configured, or invalid. Please run configure operation."}
+  end
+  if sobject.custom_metadata['api_key'] == nil or sobject.custom_metadata['base_url'] == nil then
+    return {result = nil, error = err, message = "Data Custodian credentials are not configured, or invalid. Please run configure operation."}
+  end
+  local secret = sobject:export().value:bytes()
+  local api_key = sobject.custom_metadata['api_key']
+  local base_url = sobject.custom_metadata['base_url']
+  local headers = { ['Content-Type'] = 'application/json', ['accept'] = 'application/json'}
+  local url = base_url .. '/kms/v2/auth/request'
+
+  local request_body = json.encode({ apiKey = api_key, secret = secret })
+  local response, err = request { method = 'POST', url = url, headers = headers, body=request_body }
+  if err ~= nil then
+    return {result = nil, error = err}
+  end
+  if response.status ~= 200 then
+    return {result = nil, error = json.decode(response.body)}
+  end
+  return {result = json.decode(response.body).accessToken, error = nil}
+end
+
+function config_kek_key(headers, base_url, kek_key_id, kek_key_version, is_transient)
+  local response, err
+
+  --Fetch the KEK
+  local url = base_url .. '/kms/v2/keys/' .. kek_key_id .. '/versions/' .. kek_key_version .. '/publicKey?outputFormat=BASE64_DER'
+  response, err = request { method = 'GET' , url = url, headers = headers, body='' }
+
+  if err ~= nil then
+    return {result = nil, kek_id = nil, error = err}
+  end
+  if response.status ~= 200 then
+    return {result = nil, kek_id = nil, error = response}
+  end
+  local json_resp = json.decode(response.body)
+  local kek_value = json_resp['publicKey']
+
+  -- Import KEK into DSM
+  local name = Blob.random { bits = 64 }:hex()
+  local sobject, err = Sobject.import { name = name, obj_type = 'RSA', value = kek_value, key_ops = {'EXPORT', 'WRAPKEY'}, transient = is_transient }
+  if sobject == nil then
+    err.message = err.message .. '. Failed to import KEK key in DSM.'
+    return {result = nil, kek_id = nil, error = err}
+  end
+  return {result = sobject, error = nil}
+end
+
+function create_wrapping_key(is_transient)
+  local name = Blob.random { bits = 64 }:hex()
+  local sobject, err = Sobject.create { name = name, obj_type = 'AES', key_size = 256, key_ops = {'EXPORT', 'WRAPKEY'}, transient = is_transient}
+  if sobject == nil then
+    err.message = err.message .. '. Failed to create wrapping key in DSM.'
+    return {result = nil, error = err}
+  end
+  return {result = sobject, error = nil}
+end
+
+function create_target_key(name, key_type, key_size)
+  local sobject = Sobject { name = name }
+  if sobject ~= nil then
+    return {result = sobject, error = nil}
+  end
+  local sobject, err = Sobject.create { name = name, obj_type = key_type, key_size = key_size, key_ops = {'EXPORT'} }
+  if sobject == nil then
+    err.message = err.message .. '. Failed to create target key in DSM.'
+    return {result = nil, error = err}
+  end
+  return {result = sobject, error = nil}
+end
+
+function rotate_target_key(name)
+  local sobject = Sobject { name = name }
+  if sobject == nil then
+    err.message = err.message .. '. Failed to rotate target key in DSM. It does not exist'
+    return {result = nil, error = err}
+  end
+  -- local sobject, err = sobject:rekey { name = name, obj_type = key_type, key_size = key_size, key_ops = {'EXPORT'} }
+  local sobject, err = sobject:rekey { name = name, key_ops = {'EXPORT'} }
+  if sobject == nil then
+    err.message = err.message .. '. Failed to rotate target key in DSM.'
+    return {result = nil, error = err}
+  end
+  return {result = sobject, error = nil}
+end
+
+function wrap_rsa(wrapping_key, target_key)
+  local wrap_response, err = wrapping_key:wrap { subject = target_key, mode = 'OAEP_MGF1_SHA256', alg = 'RSA'}
+  if wrap_response == nil then
+    err.message = err.message .. '. Failed to wrap key : ' .. target_key.name .. ' with wrapping key ' .. wrapped_key.name
+    return {result = nil, error = err}
+  end
+  return {result = wrap_response.wrapped_key:bytes(), error = nil}
+end
+
+function wrap_aes(wrapping_key, target_key)
+  local wrap_response, err = wrapping_key:wrap { subject = target_key, mode = 'KWP', alg = 'AES', key_format = 'Pkcs8' }
+  if wrap_response == nil then
+    err.message = err.message .. '. Failed to wrap key : ' .. target_key.name .. ' with wrapping key ' .. wrapping_key.name
+    return {result = nil, error = err}
+  end
+  return {result = wrap_response.wrapped_key:bytes(), error = nil}
+end
+
+function get_wrapped_byok_value(target_key_type, wrapped_wrapping_key, wrapped_target_key)
+  local byok_value
+  if target_key_type == 'RSA' then
+    byok_value = Blob.from_bytes(wrapped_wrapping_key.result .. wrapped_target_key.result):base64()
+  elseif target_key_type == 'AES' then
+    byok_value = Blob.from_bytes(wrapped_target_key.result):base64()
+  end
+  return byok_value
+end
+
+function perform_byok(headers, base_url, wrapped_key_value, input)
+  local key_operations
+  if input.target_key_operations == nil or input.target_key_operations == "" then
+    -- Use default key operations based on key type
+    key_operations = { 'ENCRYPT', 'DECRYPT', 'SIGN', 'VERIFY', 'WRAP', 'UNWRAP' }
+    if input.target_key_type == 'AES' then
+        key_operations = { 'ENCRYPT', 'DECRYPT', 'WRAP', 'UNWRAP' }
+    end
+  else
+    key_operations = input.target_key_operations
+  end
+
+  local algorithm = 'RSA_AES_WRAP_SHA256'
+  if input.target_key_type == 'AES' then
+    algorithm = 'OAEP_MGF1_SHA256'
+  end
+
+  -- Uwrap API Reference : https://api-kms-v2-preprod.datacustodian.cloud.sap/kms/v2/ui/#/Cryptography/unwrap
+  local request_body
+  if input.operation == 'import' then
+    -- Import operation - Call unwrap API with schema for "UnwrapNewKey"
+    request_body = json.encode({ 
+        algorithm = algorithm, 
+        keyId = input.kek_key_id,
+        version = input.target_key_version,
+        wrappedKey = wrapped_key_value,
+        target = {
+            name = input.target_key_name,
+            description = input.target_key_description,
+            groupId = input.datacustodian_group_id,
+            type = input.target_key_type,
+            exportable = input.is_exportable,
+            enabled = true,
+            operations = key_operations,
+            role = input.datacustodian_role,
+        }
+    })
+  else
+    -- Rotate operation - Call unwrap API with schema for "UnwrapKeyVersion"
+    request_body = json.encode({ 
+        algorithm = algorithm, 
+        keyId = input.kek_key_id,
+        version = input.target_key_version,
+        targetKeyId = input.datacustodian_key_id,
+        wrappedKey = wrapped_key_value
+    })
+  end
+
+  local url = base_url .. '/kms/v2/crypto/unwrap'
+  local response, err = request { method = "POST", url = url, headers = headers, body=request_body }
+  if err ~= nil then
+    return {result = nil, error = err}
+  end
+  if response.status ~= 200 then
+    return {result = nil, error = response}
+  end
+
+  return {result = json.decode(response.body), error = nil}
+end
+
+function is_valid(operation)
+  local opr = {'configure', 'import', 'rotate'}
+  for i=1,#opr do
+    if opr[i] == operation then
+      return true
+    end
+  end
+  return false
+end
+
+function is_valid_target_key_type(key_type)
+  local key_types = {'AES', 'RSA'}
+  for i=1,#key_types do
+    if key_types[i] == key_type then
+      return true
+    end
+  end
+  return false
+end
+
+function is_valid_dsm_key(name)
+  local response, err = Sobject {name = name}
+  if err ~=nil or response == nil then
+    return false
+  end
+  return true
+end
+
+function check(input)
+  if input.operation == 'configure' then
+    if input.api_key == nil then
+      return nil, 'input parameter api_key required'
+    end
+    if input.secret == nil then
+      return nil, 'input parameter secret required'
+    end
+    if input.base_url == nil then
+      return nil, 'input parameter base_url required'
+    end
+  elseif input.operation == 'import' or input.operation == 'rotate' then
+    if input.secret_id == nil or input.secret_id == "" then
+      return nil, 'input parameter secret_id required'
+    end
+    if input.kek_key_id == nil or input.kek_key_id == "" then
+      return nil, 'input parameter kek_key_id required'
+    end
+    if input.kek_key_version == nil or input.kek_key_version == "" then
+      return nil, 'input parameter kek_key_version required'
+    end
+    if input.target_key_name == nil or input.target_key_name == "" then
+      return nil, 'input parameter target_key_name required'
+    end
+    if input.target_key_version == nil or input.target_key_version == "" then
+      return nil, 'input parameter target_key_version required'
+    end
+  end
+  if input.operation == 'import' then
+    if input.datacustodian_group_id == nil or input.datacustodian_group_id == "" then
+      return nil, 'input parameter datacustodian_group_id required'
+    end
+    if input.target_key_type == nil or input.target_key_type == "" then
+      return nil, 'input parameter target_key_type required'
+    else
+      if not is_valid_target_key_type(input.target_key_type) then
+        return {result = nil, error = "Target key type is not valid. Target key type should be one of `RSA`, `AES`"}
+      end
+    end
+    if input.target_key_description == nil or input.target_key_description == "" then
+      return nil, 'input parameter target_key_description required'
+    end
+    if input.target_key_size == nil or input.target_key_size == "" then
+      return nil, 'input parameter target_key_size required'
+    end
+    if input.is_exportable == nil or input.is_exportable == "" then
+      return nil, 'input parameter is_exportable required'
+    end
+  elseif input.operation == 'rotate' then
+    if input.datacustodian_key_id == nil or input.datacustodian_key_id == "" then
+      return nil, 'input parameter datacustodian_key_id required'
+    end
+  end
+end
+
+function run(input)
+  if not is_valid(input.operation) then
+    return {result = nil, error = "Operation is not valid. Operation value should be one of `configure`, `import`, `rotate`."}
+  end
+
+  if input.operation == 'configure' then
+    return configure_credentials(input.api_key, input.secret, input.base_url)
+  else
+    local resp, err, message = login(input.secret_id)
+    if resp.result == nil then
+      return {result = resp, error = err, message = message}
+    end
+    local base_url = Sobject { id = input.secret_id }.custom_metadata['base_url']
+    local authorization_header = 'Bearer ' ..resp.result
+    headers = {['Content-Type'] = 'application/json', ['Authorization'] = authorization_header}
+    if input.operation == 'import' or input.operation == 'rotate' then
+      if input.operation == 'create' and is_valid_dsm_key(input.target_key_name) then
+        return "Error in creating target key or the key already exists in DSM."
+      end
+      if input.operation == 'rotate' and not is_valid_dsm_key(input.target_key_name) then
+        return "The key does not exist in DSM. Can not rotate. For rotate operation the key specified by target_key_name must already exist in DSM and the key specified by datacustodian_key_id must already exist in Data custodian"
+      end
+
+      local is_transient = true
+
+      -- Get KEK and import into DSM
+      local config_kek_key_resp = config_kek_key(headers, base_url, input.kek_key_id, input.kek_key_version, is_transient)
+      if config_kek_key_resp.result == nil then
+        return config_kek_key_resp
+      end
+
+      local target_key
+
+      if input.operation == 'import' then
+        -- create target key - we will import this target key in data custodian
+        target_key = create_target_key(input.target_key_name, input.target_key_type, input.target_key_size)
+      else
+        -- Rotate target key in DSM - we will import the new rotated key in data custodian
+        target_key = rotate_target_key(input.target_key_name)
+        input.target_key_type = target_key.obj_type
+      end
+      if target_key.result == nil then
+        return target_key
+      end
+
+      if input.operation == 'rotate' then
+        input.target_key_type = target_key.result.obj_type
+      end
+
+      local wrapped_wrapping_key = nil
+      local wrapped_target_key = nil
+      if input.target_key_type == 'RSA' then
+        -- create ephemeral wrapping key used to wrap target key
+        local wrapping_key = create_wrapping_key(is_transient)
+        if wrapping_key.result == nil then
+            return wrapping_key
+        end
+        -- wrap ephemeral wrapping key with kek key
+        wrapped_wrapping_key = wrap_rsa(config_kek_key_resp.result, wrapping_key.result)
+        if wrapped_wrapping_key.result == nil then
+            return wrapped_wrapping_key
+        end
+        -- Wrap target key with ephemeral key
+        wrapped_target_key = wrap_aes(wrapping_key.result, target_key.result)
+        if wrapped_target_key.result == nil then
+            return wrapped_target_key
+        end
+      elseif input.target_key_type == 'AES' then
+        -- wrap target key with kek key
+        wrapped_target_key = wrap_rsa(config_kek_key_resp.result, target_key.result)
+        if wrapped_target_key.result == nil then
+            return wrapped_target_key
+        end
+      end
+
+      local wrapped_byok_value = get_wrapped_byok_value(input.target_key_type, wrapped_wrapping_key, wrapped_target_key)
+      local response = perform_byok(headers, base_url, wrapped_byok_value, input)
+      -- Delete if non transient KEK and Wrapping keys were created
+      if not is_transient then
+        config_kek_key_resp.result:delete()
+        wrapping_key.result:delete()
+      end
+      return response
+    else
+      return {result = '', error = "Operation is not valid. Operation value should be one of `configure`, `import`, or `rotate`."}
+    end
+  end
+end
