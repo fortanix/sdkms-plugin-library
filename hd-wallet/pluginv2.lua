3ab32d2e5ac588f75b7b5f1f99ffbd9987946d0d
Jordan Rego <jordan.rego@erisx.com>
2022-07-13 10:37:02 -0500

    remove trailing whitespaces and update changelog

--- hd-wallet/pluginv2.lua
+++ hd-wallet/pluginv2.lua
@@ -1,31 +1,26 @@
 -- Name: HD Wallet
 -- Version: 2.1
--- Description:## Short Description
--- This plugin implements hierarchical deterministic wallets (or "HD Wallets") BIP0032 protocol.
---  
--- ### ## Introduction
+-- ## Description:
+--   This plugin implements hierarchical deterministic wallets (or "HD Wallets") BIP0032 protocol.
+-- ## Introduction
 -- The plugin allows to derive child key (xprv, xpub) from a master key in a deterministic way, and/or sign transaction hashes for UTXO and ethereum type crypto coin.
--- 
 -- ## Use cases
 -- The plugin can be used to
--- 
--- - Derive child key for UTXO
--- - Derive child key for ethereum
--- - Derive child key for XRP
---
--- - Sign transaction for UTXO
--- - Sign transaction for ethereum
--- - Sign transaction for XRP
--- 
+-- - Derive
+--   - Derive child key for UTXO
+--   - Derive child key for ethereum
+--   - Derive child key for XRP
+-- - Sign
+--   - Sign transaction for UTXO
+--   - Sign transaction for ethereum
+--   - Sign transaction for XRP
 -- ## Setup
 -- - Generate HD-Wallets master key manually
 -- **Example Master Key:** `xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U` 
 -- - Import master key in SDKMS as secret raw key
 -- 
 -- ## Input/Output JSON object format
--- 
 -- **Input**
--- 
 -- ```
 -- {
 --     "master_key_id": "<Master-Key-UUID>",
@@ -34,16 +29,13 @@
 --     "coin": "<Coin-Type>"
 -- }
 -- ```
--- 
 -- **Output**
--- 
 -- ```
 --  "xprv": "<HD-Wallet-Private-Key>",
 --  "xpub": "<HD-Wallet-Public-Key>",
 --  "coin_signature": "<Bitcoin-canonicalized-ECDSA-signature>",
 --  "signature": "<ECDSA signature>"
 -- ```
--- 
 -- * `master_key_id`: UUID of master key imported in SDKMS
 -- * `path`: Path of key to be derived to sign e.g: m/0, m/1, m/2/10 etc
 -- * `msg_hash`: 32 byte SHA-3 message hash
@@ -52,10 +44,8 @@
 -- * `xpub`: BIP0032 public key
 -- * `coin_signature`: Bitcoin canonicalized ECDSA signature
 -- * `signature`: ECDSA signature
--- 
 -- ## Example Input/Output JSON object for UTXO COIN (BTC, LTC, BCH, etc.)
 -- **Input JSON object**
--- 
 -- ```
 -- {
 --    "master_key_id": "0eae8ff0-553e-4f47-bb64-7c87f34bf5e5",
@@ -64,7 +54,6 @@
 --    "msg_hash": "45a0ee821b05400f513891bbb567a99139f3df72e9e1d4b48186841cc5996d2f"
 -- }
 -- ```
--- 
 -- **Output JSON object**
 -- ```
 -- {
@@ -74,7 +63,6 @@
 --   "coin_signature": "af9bf94c4959328b56861ca5f175b5e59014cb5bd2a5fcee2e95b1563dbc652e411ff01751af64d6b7209908fc58f527b07a0a9258eee7be7aa5704136954b02"
 -- }
 -- ```
--- 
 -- ## Example Input JSON object for ETH
 -- ```
 -- {
@@ -83,7 +71,6 @@
 --    "msgHash": "45a0ee821b05400f513891bbb567a99139f3df72e9e1d4b48186841cc5996d2f"
 -- }
 -- ```
---
 -- ## Example Input JSON object for XPRV 
 -- ```
 -- {
@@ -93,17 +80,16 @@
 --    "name": "Imported Key"
 -- }
 -- ```
---
 -- ## References
 -- - https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
 -- - https://xrpl.org/sign.html
--- 
 -- ### Release Notes
---  V 1.0 Initial Release
---
+--  V 1.0 
+--  - Initial Release
 --  V 2.0 
 --  - Added XRP Transaction Signature Support
 --  - Added Private Key flag and checking
+--  - Added Eth, BCH, BTC test cases
 ---------- @@@@@@@@@@@@@@@@@@@@@@@@@ ----------
 ----------      COMMON UTILITIES     ----------
 ---------- @@@@@@@@@@@@@@@@@@@@@@@@@ ----------
