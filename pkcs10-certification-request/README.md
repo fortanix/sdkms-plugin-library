18fd53016d7ec67202ebd06c1c979a6a8514fcce
Devesh Kumar <devesh.kumar@fortanix.com>
2022-11-24 15:47:28 +0530

    Add pkcs10 certificate plugin

---
+++ pkcs10-certification-request/README.md
@@ -0,0 +1,65 @@
+# PKCS #10 Certification Request
+
+## Introduction
+This plugin can produce PKCS #10 Certfication Request for a asymmetric security object
+in DSM. The sobject needs to be in a group to which this plugin has access.
+See [RFC 2986](https://datatracker.ietf.org/doc/html/rfc2986) for more details about PKCS #10.
+
+## Input/Output
+
+This plugin accepts a JSON object with the following fields as input:
+
+* `subject_dn`: a map of OIDs to values
+* `subject_key`: the name of the subject key security object
+* `attributes` (optional): a map of OIDs to an object which contains more information about that attribute.
+* `hash_alg` (optional): hash algorithm used before CSR signing.
+
+It returns the newly generated certificate signing request (CSR) in PEM format encoded as a JSON string.
+
+## Example Usage
+Assuming there exists an sobject of type RSA/EC named "Subject key", following will produce a CSR:
+
+```json
+{
+  "subject_dn": {
+    "CN": "localhost",
+    "OU": [
+      "Testing",
+      "Testing2"
+    ]
+  },
+  "subject_key": "Subject key",
+  "attributes": {
+    "extensionRequest": {
+      "subjectAlternativeName": {
+        "dns_names": [
+          "example.com",
+          "example2.com"
+        ],
+        "critical": true
+      },
+      "<any_other_extension_oid>": {
+        "der_value": "<base64_der_value>"
+      }
+    },
+    "<any_other_attribute_oid>": {
+      "der_values": ["<base64_der_value_1>", "<base64_der_value_2>", ...]
+    }
+  }
+}
+```
+
+As shown above, for some attributes, special syntax is available (only `extensionRequest` for now). Value for other attributes
+can be specified by `der_values`. Also, if `der_values` is specified for attributes with special syntax support (like `extensionRequest`),
+that special syntax will be ignored and `der_values` will be prioritised. `der_values` is an array of base64 DER encoded elements for the particular attribute.
+The input is a sequence because PKCS #10 RFC allows an attribute to have multiple values. Whether it makes sense to have multiple values or not
+depends on that particular attribute. Like `extensionRequest` only allows a single value.
+
+Also, special syntax could be available for extensions specified within `extensionRequest` attribute. If not, `der_value` can be
+used and will also be prioritised over special syntax like above. Currently, special syntax in extensions is only supported for subjectAlternativeName
+as shown above and it only supports `dns_names` for now.
+
+## References
+- [RFC 5280](https://datatracker.ietf.org/doc/html/rfc5280)
+- [RFC 2986](https://datatracker.ietf.org/doc/html/rfc2986)
+- [RFC 2985](https://datatracker.ietf.org/doc/html/rfc2985)
---
+++ pkcs10-certification-request/plugin.lua
@@ -0,0 +1,153 @@
+function check(input)
+   if type(input) ~= 'table' then
+      return nil, 'invalid input'
+   end
+
+   if input.subject_dn == nil then
+      return nil, 'must provide subject DN'
+   end
+
+   if input.subject_key == nil then
+      return nil, 'must specify subject key'
+   end
+
+   if input.attributes ~= nil and type(input.attributes) ~= 'table' then
+      return nil, 'attributes must be a table'
+   end
+
+   if input.hash_alg ~= nil and type(input.hash_alg) ~= 'string' then
+      return nil, 'digest must be a string'
+   end
+end
+
+function format_pem(b64, type)
+   local wrap_at = 64
+   local len = string.len(b64)
+   local pem = ""
+
+   pem = pem .. "-----BEGIN " .. type .. "-----\n"
+
+   for i = 1, len, wrap_at do
+      local stop = i + wrap_at - 1
+      pem = pem .. string.sub(b64, i, stop) .. "\n"
+   end
+
+   pem = pem .. "-----END " .. type .. "-----\n"
+
+   return pem
+end
+
+function load_dn(dn)
+   local name = X509Name.new()
+
+   for k,v in pairs(dn)
+   do
+      -- if there are multiple attributes in a Name,
+      -- they will get overwritten in case of a dictionary.
+      -- So, to allow specifying multiple attrs, we allow
+      -- specifying a list of values instead of a single value.
+      -- like "OU": ["OU1", "OU2"].
+      if type(v) == 'table'
+      then
+         for _, v in ipairs(v)
+         do
+            name:set(Oid.from_str(k), v, 'utf8')
+         end
+      else
+         name:set(Oid.from_str(k), v, 'utf8')
+      end
+   end
+
+   return name
+end
+
+function add_san(csr_builder, critical, extension_info)
+   local dns_names = extension_info.dns_names
+   if type(dns_names) ~= 'table' then
+      return nil, 'dns_names should be a list'
+   end
+
+   local der_encoded_value_builder = DerEncoder.new():start_seq()
+
+   for _, dns_name in ipairs(dns_names)
+   do
+      der_encoded_value_builder = der_encoded_value_builder
+         :implicit_context(2)
+         :add_string(dns_name, 'ia5')
+   end
+
+   local der_encoded_value = der_encoded_value_builder:end_seq():value()
+
+   csr_builder:add_extension(Oid.from_str('subjectAlternativeName'), critical, der_encoded_value)
+end
+
+function add_csr_extension(csr_builder, extension, extension_info)
+   if type(extension) ~= "string" then
+      return nil, 'extension oid must be string'
+   end
+
+   if type(extension_info) ~= "table" then
+      return nil, extension .. ' must be described with a table'
+   end
+
+   local critical = extension_info.critical
+
+   if critical == nil then
+      critical = false
+   elseif type(critical) ~= 'boolean' then
+      return nil, 'critical should be a bool'
+   end
+
+   if extension_info.der_value ~= nil then
+      local der_value = Blob.from_base64(extension_info.der_value):bytes()
+      csr_builder:add_extension(Oid.from_str(extension), critical, der_value)
+   elseif extension == 'subjectAlternativeName' then
+      add_san(csr_builder, critical, extension_info)
+   else
+      return nil, '`der_value` needed for ' .. extension
+   end
+end
+
+function add_csr_attribute(csr_builder, attribute, attribute_info)
+   if type(attribute) ~= "string" then
+      return nil, 'attribute name must be string'
+   end
+
+   if type(attribute_info) ~= "table" then
+       return nil, 'attribute info must be a table'
+   end
+
+   if attribute_info.der_values ~= nil then
+      for _, der_value in ipairs(attribute_info.der_values) do
+         local der_value = Blob.from_base64(der_value):bytes()
+         csr_builder:append_attribute(Oid.from_str(attribute), der_value)
+      end
+   elseif attribute == 'extensionRequest' then
+      for extension, extension_info in pairs(attribute_info) do
+         add_csr_extension(csr_builder, extension, extension_info)
+      end
+   else
+      return nil, '`der_values` needed for ' .. attribute
+   end
+end
+
+function run(input)
+   local subject_dn = load_dn(input.subject_dn)
+
+   local csr_builder = Pkcs10Csr.builder()
+
+   if input.hash_alg ~= nil then
+      csr_builder:with_hash_alg(input.hash_alg)
+   end
+
+   csr_builder:with_subject(subject_dn)
+   csr_builder:with_signer_sobject(input.subject_key)
+
+   for attribute, attribute_info in pairs(input.attributes) do
+      add_csr_attribute(csr_builder, attribute, attribute_info)
+   end
+
+   local csr = csr_builder:build()
+
+   return format_pem(Blob.from_bytes(csr:to_der()):base64(), "CERTIFICATE REQUEST")
+end
